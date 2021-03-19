ecac4552328de1619d71d3f758cc0ecf5e075495
SanipRijal <saneprijal@gmail.com>
2021-03-19 16:13:54 +0545

    generator functions for client id and secret key

--- main/settings.py
+++ main/settings.py
@@ -14,6 +14,10 @@ import os
 
 from pathlib import Path
 
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,10 +31,27 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'rtkfo9ok4jxp-%v3u46xiwm^#%=scfxc0ga*m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
+AUTH_PROVIDER = {
+    "CLIENT_ID_GENERATOR_CLASS": "provider.generators.ClientIdGenerator",
+    "CLIENT_SECRET_GENERATOR_CLASS": "provider.generators.ClientSecretGenerator"
+}
+
+USER_SETTINGS = getattr(settings, 'AUTH_PROVIDER', None)
+
+DEFAULTS = {
+    "CLIENT_ID_GENERATOR_CLASS": "provider.generators.ClientIdGenerator",
+    "CLIENT_SECRET_GENERATOR_CLASS": "provider.generators.ClientSecretGenerator",
+    "CLIENT_SECRET_GENERATOR_LENGTH": 128,
+}
+
+IMPORT_STRINGS = (
+    "CLIENT_ID_GENERATOR_CLASS",
+    "CLIENT_SECRET_GENERATOR_CLASS",
+)
+
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -118,3 +139,69 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+def perform_import(val, setting_name):
+    """
+    If the given setting is a string import notation,
+    then perform the necessary import or imports.
+    """
+    if val is None:
+        return None
+    elif isinstance(val, str):
+        return import_from_string(val, setting_name)
+    elif isinstance(val, (list, tuple)):
+        return [import_from_string(item, setting_name) for item in val]
+    return val
+
+
+def import_from_string(val, setting_name):
+    """
+    Attempt to import a class from a string representation.
+    """
+    try:
+        return import_string(val)
+    except ImportError as e:
+        msg = "Could not import %r for setting %r. %s: %s." % (val, setting_name, e.__class__.__name__, e)
+        raise ImportError(msg)
+
+class ProviderSettings:
+    """
+    A setting object that allows provider settings to be accessed as properties.
+    """
+
+    def __init__(self, user_settings=None, defaults=None, import_strings=None):
+        self._user_settings = user_settings or {}
+        self.defaults = defaults or DEFAULTS
+        self.import_strings = import_strings or IMPORT_STRINGS
+
+    @property
+    def user_settings(self):
+        if not hasattr(self, "_user_settings"):
+            self._user_settings = getattr(settings, "AUTH_PROVIDER", {})
+        return self._user_settings
+
+    def __getattr__(self, attr):
+        if attr not in self.defaults:
+            raise AttributeError("Invalid Provider setting: %s" % attr)
+
+        try:
+            # check if present in user settings
+            val = self._user_settings[attr]
+        except KeyError:
+            # Fall back to defaults
+            val = self.defaults[attr]
+
+        if val and attr in self.import_strings:
+            val = perform_import(val, attr)
+        self.validate_setting(attr, val)
+        setattr(self, attr, val)
+        return val
+
+    def validate_setting(self, attr, val):
+        if not val and attr in self.mandatory:
+            raise AttributeError("OAuth2Provider setting: %s is mandatory" % attr)
+        return self.defaults[attr]
+
+
+auth_settings = ProviderSettings(USER_SETTINGS, DEFAULTS, IMPORT_STRINGS)
