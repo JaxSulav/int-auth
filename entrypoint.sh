60433d14a13ce52cda07126820d2648a71db24db
SanipRijal <saneprijal@gmail.com>
2022-01-03 09:02:54 +0545

    use nginx to serve auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
->&2 echo "Waiting for database to start"
+#>&2 echo "Waiting for database to start"
 
 # wait for the postgres docker to be running
-while ! nc db 5432; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 0.1
-done
-
->&2 echo "Postgres is up - executing command"
+#until psql -h db -U "postgres" -c '\l'; do
+#  >&2 echo "Postgresql is unavailable - sleeping"
+#  sleep 1
+#done
+#
+#>&2 echo "Postgres is up - executing command"
 
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
 
-# # Start server
-echo "Starting server"
-gunicorn main.wsgi:application --bind 0.0.0.0:8000 --reload
+echo "Starting application"
+gunicorn main.wsgi:application --bind 0.0.0.0:8000 --reload --log-level debug
